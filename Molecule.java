569c6309a4f4ae46e61e3667466b0906937d18b7
Lexie Kemp <lexiekemp@Audreys-MacBook-Air-2.local>
2017-06-18 14:12:19 -0700

    added ID to molecule

--- Molecule.java
+++ Molecule.java
@@ -6,14 +6,16 @@ public class Molecule {
 	private double radius;
 	private double viscosity; //the amount that neighbors affect it (from 0 to 1)
 	private double range; //the distance between boundaries in which molecules count as neighbors
+	private int id;
 	
-	public Molecule(Vector initPos, Vector initVel, double newMass, double newRadius, double newViscosity, double newRange) {
+	public Molecule(Vector initPos, Vector initVel, double newMass, double newRadius, double newViscosity, double newRange, int newId) {
 		position = initPos;
 		velocity = initVel;
 		mass = newMass;
 		radius = newRadius;
 		viscosity = newViscosity;
 		range = newRange + radius;
+		id = newId;
 	}
 	
 	public Vector getPosition() {
