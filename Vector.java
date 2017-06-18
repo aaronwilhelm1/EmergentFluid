b1c2444de724c2f4034f126bf37a7b633bf8cad0
Lexie Kemp <lexiekemp@Audreys-MacBook-Air-2.local>
2017-06-17 22:09:33 -0700

    improved readablity for Vector Class

--- Vector.java
+++ Vector.java
@@ -1,47 +1,40 @@
-
-/**
- * Write a description of class Vector here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Vector
 {
     private double myX;
     private double myY;
-    public Vector()
-    {
+    
+    public Vector() {
         myX=0.0;
         myY=0.0;
     }
-    public Vector(double x, double y)
-    {
+    
+    public Vector(double x, double y) {
         myX=x;
         myY=y;
     }
-    public double getX()
-    {
+    
+    public double getX() {
         return myX;
     }
-    public double getY()
-    {
+    
+    public double getY() {
         return myY;
     }
-    public void setX(double newX)
-    {
+    
+    public void setX(double newX) {
         myX=newX;
     }
-    public void setY(double newY)
-    {
+    
+    public void setY(double newY) {
         myY=newY;
     }
-    public void setCoords(double x, double y)
-    {
+    
+    public void setCoords(double x, double y) {
         myX=x;
         myY=y;
     }
-    public double dot(Vector other)
-    {
+    
+    public double dot(Vector other) {
         return myX*other.getX()+myY*other.getY();
     }
 }
