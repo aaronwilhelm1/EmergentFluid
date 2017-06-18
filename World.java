0abeedb9d70319eb5c5e74262e29dc469b95d389
Lexie Kemp <lexiekemp@Audreys-MacBook-Air-2.local>
2017-06-18 14:40:48 -0700

    copied molecule array

--- World.java
+++ World.java
@@ -1,39 +1,49 @@
 import java.util.*;
-import java.lang.Math;
 public class World {
     private ArrayList<Molecule> molecules;
-    public final int molCount=20;
-    public final double gravity=9.8;
-
+    public final int MOLCOUNT=20;
+    public final double GRAVITY=9.8;
+    public final int COLLISION=0;
+    public final int NEIGHBOR=1;
+    public final int OUT_OF_RANGE=2;
+    private static int ID=0;
     public World() {
         molecules=new ArrayList<Molecule>();
         Vector startPos=new Vector((double)GUI.WINDOW_WIDTH/2.0, (double)GUI.WINDOW_HEIGHT/2.0);
         Vector startVel=new Vector(0.0, 0.0);
-        for (int x=0;x<molCount;x++) {
-            molecules.add(new Molecule(startpPos, startVel, 1, 1, 1, 1));
+        
+        for (int x=0;x<MOLCOUNT;x++) {
+            molecules.add(new Molecule(startpPos, startVel, 1, 1, 1, 1, ID));
+            ID++;
         }
     }
     
     public void step(double timeElapsed)
     {
-        ArrayList<Molecule> newMols=molecules;
+        ArrayList<Molecule> newMols=new ArrayList<Molecule>(); //list of molecules to be edited
+        
+        for (Molecule old : molecules) {
+            newMols.add(new Molecule(old.getPosition(), old.getVelocity(), old.getMass(), old.getRadius(), old.getViscosity(), old.getRange(), old.getId()));
+        }
+        
         for (Molecule mol : newMols) {
             //account for accelerations
-            double newXVel=mol.getVelocity.getX()+gravity*timeElapsed;
-            double newYVel=mol.getVelocity.getY()+gravity*timeElapsed;
+            double newXVel=mol.getVelocity.getX()+GRAVITY*timeElapsed;
+            double newYVel=mol.getVelocity.getY()+GRAVITY*timeElapsed;
             mol.setVelocity(new Velocity(newXVel, newYVel));
 
             ArrayList<Molecule> neighbors=new ArrayList<Molecule>();
             //find neighbors
             for (Molecule other : molecules) {
-                if (interact(mol, other)==0) {
-                   collide(mol, other); 
-                }
-                if (interact(mol, other)==1) {
-                    neighbors.add(other); //MAKE SURE NOT TO ADD IF OTHER MOLECULE IS SELF (does it wokr to do if mol!=other ?)
+                if (other.getId() != mol.getId()) {//not same molecule
+                    if (interact(mol, other)==COLLISION) { //check for collisions
+                        collide(mol, other); 
+                    }
+                    if (interact(mol, other)==NEIGHBOR) { //check for nearby molecules
+                        neighbors.add(other); 
+                    }
                 }
             }
-            //check for collisions
             //change velocities based on vicosity
         }
         molecules=newMols; //set list of molecules to updated molecules
@@ -42,18 +52,18 @@ public class World {
     public double getDist(Molecule one, Molecule two) {
         double xDist=one.getPosition.getX()-two.getPosition.getX();
         double yDist=one.getPosition.getY()-two.getPosition.getY();
-        return hypot(xDist, yDist);
+        return getMagnitude(xDist, yDist);
     }
     
     public int interact(Molecule one, Molecule two) {
         double dist=getDist(one,two);
         if (dist<=(one.getRadius()+two.getRadius())) {
-            return 0; //collision
+            return COLLISION; 
         }
         if (dist<=(one.getRange()+two.getRadius())) {
-            return 1; //neighbor
+            return NEIGHBOR; 
         }
-        return 2; //out of range
+        return OUT_OF_RANGE; 
     }
     
     public void collide(Molecule one, Molecule two) {
